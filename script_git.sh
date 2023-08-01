fd718bb6961fe0d489a02be58ffe24954e7b1d3d
Rookieason <eason20010910@gmail.com>
2023-08-01 16:46:45 +0800

    from /c/Users/user/AppData/Roaming/SPB_Data

--- script_git.sh
+++ script_git.sh
@@ -1,4 +1,6 @@
 git add -A
 git commit -m "$Update from $HOME"
-git pull https://GaryChenMesser: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@github.com/GaryChenMesser/dash.js.git
-git push https://GaryChenMesser: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@github.com/GaryChenMesser/dash.js.git
+# git pull https://GaryChenMesser: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@github.com/GaryChenMesser/dash.js.git
+# git push https://GaryChenMesser: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@github.com/GaryChenMesser/dash.js.git
+git pull git@github.com:Rookieason/dash.js.git
+git push git@github.com:Rookieason/dash.js.git
